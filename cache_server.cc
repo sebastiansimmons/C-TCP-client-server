ef6ce5b4af766981089a56d237c8d98c3ef3717e
Sebastian Simmons <sebastianctsimmons@gmail.com>
2020-03-21 14:25:47 -0700

    updated arg parsing on cache_server.cc

--- cache_server.cc
+++ cache_server.cc
@@ -1,25 +1,25 @@
 /*
 tcp_server.cc 
 */
-#include<iostream>
-#include<stdio.h>
-#include<ctype.h>
-#include<stdlib.h>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include <unistd.h>
-using namespace std;
 
 int main(int argc, char **argv){
     int opt,m,p,t;
-    string s;
+    std::string s;
     while ((opt = getopt(argc,argv,"m:s:p:t:d")) != EOF)
         switch(opt)
         {
-            case 'm': m = 1; cout << "cache size set to "<< optarg <<endl ; break;
-            case 's': s = 1; cout << "running server on "<< optarg <<endl ; break;
-            case 'p': p = 1; cout << "with port "<< optarg <<endl ; break;
-            case 't': t = 1; cout << "on "<< optarg << " threads" <<endl ; break;
+            case 'm': m = 1; std::cout << "cache size set to "<< optarg << std::endl ; break;
+            case 's': s = 1; std::cout << "running server on "<< optarg << std::endl ; break;
+            case 'p': p = 1; std::cout << "with port "<< optarg << std::endl ; break;
+            case 't': t = 1; std::cout << "on "<< optarg << " threads" << std::endl ; break;
             default: /* '?' */
-                   fprintf(stderr, "Usage: %s [-m bytes] maxmem [-s] server ip\n",
+                   fprintf(stderr, "Usage: %s [-m <bytes>] maxmem [-s <ip>] server ip [-p <int>] port number [-t <int>] number of threads\n",
                            argv[0]);
                    exit(EXIT_FAILURE);
         }
