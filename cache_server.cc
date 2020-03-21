485a7e895dc391291c7717fd8a28bb5f3278558c
3141592654 <12554811+3141592654@users.noreply.github.com>
2020-03-21 14:31:31 -0700

    This is what I have been up to for the past day.

--- cache_server.cc
+++ cache_server.cc
@@ -8,6 +8,287 @@ tcp_server.cc
 #include <stdlib.h>
 #include <unistd.h>
 
+// Unless specified otherwise, assume the following code was adapted from
+// https://www.boost.org/doc/libs/1_72_0/libs/beast/example/http/server/async/http_server_async.cpp
+// Because this is an asynchronous server using TCP like the prompt requests.
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/config.hpp>
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+// Declare global cache variable, initialise it in main
+Cache* test_cache;
+
+
+// Almost everything we have to implement is in the following function.
+
+// This function produces an HTTP response for the given
+// request. The type of the response object depends on the
+// contents of the request, so the interface requires the
+// caller to pass a generic lambda for receiving the response.
+template<
+    class Body, class Allocator,
+    class Send>
+void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+    // BEGIN STUFF I THINK WE CAN IGNORE
+    // Returns a bad request response
+    auto const bad_request =
+    [&req](beast::string_view why)
+    {
+        http::response<http::string_body> res{http::status::bad_request, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = std::string(why);
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a not found response
+    auto const not_found =
+    [&req](beast::string_view target)
+    {
+        http::response<http::string_body> res{http::status::not_found, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "The resource '" + std::string(target) + "' was not found.";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a server error response
+    auto const server_error =
+    [&req](beast::string_view what)
+    {
+        http::response<http::string_body> res{http::status::internal_server_error, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "An error occurred: '" + std::string(what) + "'";
+        res.prepare_payload();
+        return res;
+    };
+    // END STUFF I THINK WE CAN IGNORE
+
+    switch (req.method()) {
+        case http::verb::get  :
+            // This doesn't compile yet.
+            /*uint32_t s = -1;    //TODO: REPLACE THIS WITH BETTER TYPING
+            const char* retp = test_cache->get(req.target(), s);
+            if (retp == nullptr) {
+                // WE RETURN AN ERROR MESSAGE IF THE KEY IS NOT IN THE CACHE
+            } else {
+                http::file_body::value_type body;
+                // The following bit is fairly directly copied.
+                http::response<http::file_body> res{std::piecewise_construct, std::make_tuple(std::move(body)), std::make_tuple(http::status::ok, req.version())};
+                res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+                //beast::string_view retval = beast::string_view(retp, s);  // NOT SURE THIS IS HOW THE INITIALISER WORKS.
+                //res.set(http::field::content_type, retval);  // See if we can ignore this.
+                res.content_length(s);
+                res.keep_alive(req.keep_alive());
+                return send(std::move(res));
+            }*/
+            // IMPLEMENT ME
+        case http::verb::put  :
+            // IMPLEMENT ME
+        case http::verb::delete_  :
+            // IMPLEMENT ME
+        case http::verb::head  :
+            // IMPLEMENT ME
+        case http::verb::post  :
+            // IMPLEMENT ME
+        default  :
+            // We support the GET, PUT, DELETE, HEAD and POST requests. If the request
+            // is of a different type, tell the user to jump in a lake.
+            return send(bad_request("Unknown HTTP-method"));
+    }
+}
+
+
+
+
+// IGNORE THIS STUFF
+// Report a failure
+void fail(beast::error_code ec, char const* what) {
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+// Handles an HTTP server connection
+class session : public std::enable_shared_from_this<session> {
+    // This is the C++11 equivalent of a generic lambda.
+    // The function object is used to send an HTTP message.
+    struct send_lambda {
+        session& self_;
+        explicit send_lambda(session& self) : self_(self) {}
+
+        template<bool isRequest, class Body, class Fields> void operator()(http::message<isRequest, Body, Fields>&& msg) const {
+            // The lifetime of the message has to extend
+            // for the duration of the async operation so
+            // we use a shared_ptr to manage it.
+            auto sp = std::make_shared<http::message<isRequest, Body, Fields>>(std::move(msg));
+
+            // Store a type-erased version of the shared
+            // pointer in the class to keep it alive.
+            self_.res_ = sp;
+
+            // Write the response
+            http::async_write(self_.stream_, *sp, beast::bind_front_handler(&session::on_write, self_.shared_from_this(), sp->need_eof()));
+        }
+    };
+
+    beast::tcp_stream stream_;
+    beast::flat_buffer buffer_;
+    http::request<http::string_body> req_;
+    std::shared_ptr<void> res_;
+    send_lambda lambda_;
+
+public:
+    // Take ownership of the stream
+    session(tcp::socket&& socket) : stream_(std::move(socket)), lambda_(*this) {}
+
+    // Start the asynchronous operation
+    void run() {
+        // We need to be executing within a strand to perform async operations
+        // on the I/O objects in this session. Although not strictly necessary
+        // for single-threaded contexts, this example code is written to be
+        // thread-safe by default.
+        net::dispatch(stream_.get_executor(), beast::bind_front_handler(&session::do_read, shared_from_this()));
+    }
+
+    void do_read() {
+        // Make the request empty before reading,
+        // otherwise the operation behavior is undefined.
+        req_ = {};
+
+        // Set the timeout.
+        stream_.expires_after(std::chrono::seconds(30));
+
+        // Read a request
+        http::async_read(stream_, buffer_, req_, beast::bind_front_handler(&session::on_read, shared_from_this()));
+    }
+
+    void on_read(beast::error_code ec, std::size_t bytes_transferred) {
+        boost::ignore_unused(bytes_transferred);
+
+        // This means they closed the connection
+        if(ec == http::error::end_of_stream) {
+            return do_close();
+        }
+        if(ec) {
+            return fail(ec, "read");
+        }
+        // Send the response
+        handle_request(std::move(req_), lambda_);
+    }
+
+    void on_write(bool close, beast::error_code ec, std::size_t bytes_transferred) {
+        boost::ignore_unused(bytes_transferred);
+
+        if(ec) {
+            return fail(ec, "write");
+        }
+        if(close) {
+            // This means we should close the connection, usually because
+            // the response indicated the "Connection: close" semantic.
+            return do_close();
+        }
+
+        // We're done with the response so delete it
+        res_ = nullptr;
+
+        // Read another request
+        do_read();
+    }
+
+    void do_close() {
+        // Send a TCP shutdown
+        beast::error_code ec;
+        stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
+
+        // At this point the connection is closed gracefully
+    }
+};
+
+
+// Accepts incoming connections and launches the sessions
+class listener : public std::enable_shared_from_this<listener> {
+    net::io_context& ioc_;
+    tcp::acceptor acceptor_;
+
+  public:
+    listener(
+        net::io_context& ioc,
+        tcp::endpoint endpoint)
+        : ioc_(ioc)
+        , acceptor_(net::make_strand(ioc)) {
+
+        beast::error_code ec;
+
+        // Open the acceptor
+        acceptor_.open(endpoint.protocol(), ec);
+        if(ec) {
+            fail(ec, "open");
+            return;
+        }
+
+        // Allow address reuse
+        acceptor_.set_option(net::socket_base::reuse_address(true), ec);
+        if(ec) {
+            fail(ec, "set_option");
+            return;
+        }
+
+        // Bind to the server address
+        acceptor_.bind(endpoint, ec);
+        if(ec) {
+            fail(ec, "bind");
+            return;
+        }
+
+        // Start listening for connections
+        acceptor_.listen(net::socket_base::max_listen_connections, ec);
+        if(ec) {
+            fail(ec, "listen");
+            return;
+        }
+    }
+
+    // Start accepting incoming connections
+    void run() {
+        do_accept();
+    }
+  private:
+    void do_accept() {
+        // The new connection gets its own strand
+        acceptor_.async_accept(net::make_strand(ioc_), beast::bind_front_handler(&listener::on_accept, shared_from_this()));
+    }
+    void on_accept(beast::error_code ec, tcp::socket socket) {
+        if(ec) {
+            fail(ec, "accept");
+        } else {
+            // Create the session and run it
+            std::make_shared<session>(std::move(socket))->run();
+        }
+        // Accept another connection
+        do_accept();
+    }
+};
+
+
 int main(int argc, char **argv){
     int opt,m,p,t;
     std::string s;
@@ -23,6 +304,16 @@ int main(int argc, char **argv){
                            argv[0]);
                    exit(EXIT_FAILURE);
         }
+
+    thread_count = 1;
+    // TODO: CLEAN THIS UP
+    auto const address = net::ip::make_address(s);
+    auto const portna = static_cast<unsigned short>(p);
+    test_cache = new Cache(m);
+    // The io_context is reportedly required for all I/O
+    net::io_context ioc{t};
+    listener* my_listener = new listener(ioc, tcp::endpoint{address, portna});
+    my_listener->run();
 
     return 0;
 }
